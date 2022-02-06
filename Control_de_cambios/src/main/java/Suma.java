838f7b4c48d4adf8712ead644a417ba54f06a5dc
JordySoftware <69678718+JordySoftware@users.noreply.github.com>
2022-02-06 14:30:03 -0500

    Add files via upload

---
+++ Control_de_cambios/src/main/java/Suma.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jordy
+ */
+
+
+class Suma {
+    
+    Scanner reader = new Scanner(System.in);
+
+ 
+   public static void main( String[] args ) {
+     int numero1 = 0;
+     int numero2 = 0;
+     int resultado = 0;
+     Scanner reader = new Scanner(System.in);
+  
+    System.out.println("Introduce el primer número:");      
+    numero1 = reader.nextInt();
+
+    System.out.println("Introduce el segundo número:");
+    numero2 = reader.nextInt();
+
+    resultado = numero1+numero2;
+
+    System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+}
+}
